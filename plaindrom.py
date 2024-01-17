e239901023ad111646ecf82ab8e2809399a8a547
Prakash <118040708+prototypesDeprakash@users.noreply.github.com>
2024-01-17 17:09:10 +0530

    Update plaindrom.py

--- plaindrom.py
+++ plaindrom.py
@@ -1,25 +1,18 @@
-x=121
+'''
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+'''
+x=-121
 x=str(x)
-main=[]
-for i in x:
-    main.append(i)
-print(main)
-
-orig =""
-revo=""
-for i in main:
-    orig+=i
-print(orig)
-
-for j in range(len(main)-1, 0 if main[0] == '-' else -1, -1):
- 
-    revo += main[j]
-
-if("-" in main):
-    revo+="-"
-print(revo)
-
-if(orig==revo):
-    print("ture")
+y=x[::-1]
+print(x)
+if(x==y):
+    print("true")
 else:
     print("false")
